ceb76c7d042163e324ef8adc5eeccb9ce773215e
ligayan <daniel.nowosad89@gmail.com>
2018-07-28 16:39:49 +0200

    1.03

---
+++ Rozdział 6 - Wprowadzenie do klas/src/Box.java
@@ -0,0 +1,18 @@
+
+public class Box {
+
+	double width;
+	double height;
+	double depth;
+
+	void volume() {
+		System.out.print("Objêtoœæ: ");
+		System.out.println(width * height * depth);
+
+	}
+	
+	double volumeRet() {
+		return width*height*depth;
+	}
+
+}
